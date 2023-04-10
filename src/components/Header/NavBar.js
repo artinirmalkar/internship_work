669de345aeacdbea106d10e86dfaf006be12cee2
HIMANxxU <himanshu321go@gmail.com>
2023-04-10 15:46:50 +0530

    REsolving merg

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -2,12 +2,14 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import 
+
 
 const NavBar = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="" alt="" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
