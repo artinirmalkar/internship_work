09afb45e62a43175be7b277d01a647030c52d513
HIMANxxU <himanshu321go@gmail.com>
2023-04-10 10:34:18 +0530

    Font Incorporated

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../static/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, error,
+        voluptatum consequatur qui itaque iste magnam modi ab unde earum
+        necessitatibus, adipisci omnis quibusdam odit aut molestiae? Officiis
+        nesciunt vel eius numquam sed, consequuntur quasi itaque hic natus,
+        cupiditate a temporibus? Voluptatem voluptas enim eos quo cum tempore
+        itaque quia?
+      </p>
+    </div>
+  );
+};
+
+export default Navbar;
