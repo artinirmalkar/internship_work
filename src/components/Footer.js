9497166df6c5fdeadf51867a42da0a6646cbbcbc
Arti610 <artinirmalkar06@gmail.comL>
2023-04-10 09:56:51 +0530

    Zaajira

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="left">
+      <h2>Zaajira</h2>
+        <p>An integrated AI talent acquisition platform, helping screen <br /> applicant volumes and connect relevant talent.</p>
+      </div>
+      <div className="right">
+        <div className="first"></div>
+        <div className="second"></div>
+        <div className="third"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
