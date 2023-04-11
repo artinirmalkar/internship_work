23d3ae707f1174147b92d6601fea04b9587e4d57
Arti610 <artinirmalkar06@gmail.comL>
2023-04-11 12:49:25 +0530

    counter fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import "./App.css";
-// import Card from "./static/Single Component/Card";
-// import CardCarousel from "./static/Single Component/CardCarousel";
+// import "./App.css";
 import NavBar from "./components/Header/NavBar";
 import Counter from "./components/Main/Counter";import Banner from "./components/Header/Banner";
-
 import TopFooter from "./components/Footer/TopFooter";
 import Footer from "./components/Footer/Footer";
-// import Profile from "./static/Single Component/Profile";
+
 import Body from "./components/Main/Body";
+// import Card from "./static/Single Component/Card";
 
 
 function App() {
@@ -20,10 +18,7 @@ function App() {
       <TopFooter />
       <Footer title="Zaajira" />
 
-      {/* <CardCarousel/> */}
-
-{/* <Card/> */}
-    
+    {/* <Card/> */}
     </>
   );
 }
--- src/static/Single Component/Card.js
+++ src/static/Single Component/Card.js
@@ -3,9 +3,9 @@ import CardCarousel from './CardCarousel'
 
 const Card = () => {
   return (
-    <div className='card'>
+    <div className='card' style={{backgroundColor:"red"}}>
       <CardCarousel/>
-      <div className="content" style={{ width:"17rem", padding:"10px"}}>
+      <div className="content" style={{ width:"17rem", padding:"0px 15px", textAlign:"justify", display:"inline-block"}}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia nobis eaque cum praesentium itaque aliquid fugiat, sapiente quasi rerum laudantium deleniti culpa. Fuga quidem labore perferendis natus quas in distinctio excepturi fugit, nobis alias obcaecati unde nulla ut blanditiis dolor aspernatur error incidunt vitae ab officiis? Dicta totam sint maxime.
       </div>
     </div>
