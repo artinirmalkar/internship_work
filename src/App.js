468dda65995c0d38137dd342ae0948a6a31ee4f4
HIMANxxU <himanshu321go@gmail.com>
2023-04-11 12:54:35 +0530

    Code Retified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-// import "./App.css";
 import NavBar from "./components/Header/NavBar";
-import Counter from "./components/Main/Counter";import Banner from "./components/Header/Banner";
+import Counter from "./components/Main/Counter";
+import Banner from "./components/Header/Banner";
 import TopFooter from "./components/Footer/TopFooter";
 import Footer from "./components/Footer/Footer";
 
-import Body from "./components/Main/Body";
 // import Card from "./static/Single Component/Card";
-
 
 function App() {
   return (
     <>
       <NavBar />
       <Banner />
-      <Body />
+
       <Counter />
       <TopFooter />
       <Footer title="Zaajira" />
 
-    {/* <Card/> */}
+      {/* <Card/> */}
     </>
   );
 }
