592b1a7ec3181015b5d7b62e17816f54d930f319
HIMANxxU <himanshu321go@gmail.com>
2023-04-11 18:04:06 +0530

    Added banner and started

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Counter from "./components/Main/Counter";
 import Banner from "./components/Header/Banner";
 import TopFooter from "./components/Footer/TopFooter";
 import Footer from "./components/Footer/Footer";
+import Features from "./components/Main/Features";
 
 // import Card from "./static/Single Component/Card";
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <NavBar />
       <Banner />
+      <Features />
 
       <Counter />
       <TopFooter />
---
+++ src/components/Main/Features.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Features.css";
+
+const Features = () => {
+  return (
+    <div className="feature">
+      <div className="feature-left">
+        <p className="zaajira-text">Why Apply On Zaajira ?</p>
+        <p className="zaajira-text-bottom">
+          Feature that supercharge your job search and profile
+        </p>
+      </div>
+      <div className="feature-right">
+        <div className="feature-content feature-content1">
+          <i class="fa-regular fa-circle-check"></i>
+          <p>Connect with top employers in minutes</p>
+        </div>
+        <div className="feature-content feature-content2">
+          <i class="fa-regular fa-circle-check"></i>
+          <p>Connect with top employers in minutes</p>
+        </div>
+        <div className="feature-content">
+          <i class="fa-regular fa-circle-check"></i>
+          <p>Connect with top employers in minutes</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
