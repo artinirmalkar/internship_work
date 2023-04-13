44a78611210c3032a559b31bad1420d55e206505
HIMANxxU <himanshu321go@gmail.com>
2023-04-13 17:40:03 +0530

    Working on counter

--- src/components/Main/Counter.js
+++ src/components/Main/Counter.js
@@ -2,11 +2,34 @@ import React, { useState } from "react";
 import "../Main/Counter.css";
 // const [Counter,setCounter]=useState({});
 
-const Counter = props => {
+const Counter = () => {
+  // const [scroll, setscroll] = useState(false);
+  const hh = () => {
+    console.log(window.scrollY, "window.screenY");
+    if (window.scrollY === 800) {
+      let counts = setInterval(updated, 10);
+      let upto = 4800;
+
+      function updated() {
+        var count = document.getElementById("counter");
+        count.innerHTML = ++upto;
+        if (upto === 5000) {
+          clearInterval(counts);
+        }
+      }
+    } else {
+      console.log(window, "ugiyhyhyhu");
+    }
+  };
+  // console{}.log(window.screenY);
+  // setscroll(125);
+  {
+    window.addEventListener("scroll", hh);
+  }
   return (
     <div className="counter">
       <div className="counterItem">
-        <h3 id="counter">5K+</h3>
+        <h3 id="counter"></h3>
         <p>Candidates</p>
       </div>
       <div className="counterItem">
