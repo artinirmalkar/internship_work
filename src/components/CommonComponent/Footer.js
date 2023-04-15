3ec0294090402b964dbe4d8181e9337e7584b35a
Arti610 <artinirmalkar06@gmail.comL>
2023-04-15 16:24:48 +0530

    Added Employer page in Footer

--- src/components/CommonComponent/Footer.js
+++ src/components/CommonComponent/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Footer.css";
 import image from "../../static/images/logo.PNG";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer row mright">
@@ -15,7 +16,7 @@ function Footer() {
       <div className="second textalign col-md-3 col-lg-2">
         <ul>
           <li>
-            <a href="#">Employers</a>
+            <Link to="/Employers">Employers</Link>
           </li>
           <li>
             <a href="#">Candidates</a>
@@ -47,17 +48,17 @@ function Footer() {
         </ul>
         <ul className="icon">
           <li>
-            <a href="#">
+            <a href="https://twitter.com/zaajira">
               <i class="fa-brands fa-twitter" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.instagram.com/zaajira.app/">
               <i class="fa-brands fa-instagram" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQGbakPG3i0P6AAAAYeD7p3QgyRNjqVbMYeBELNJ1ErW32hPc_DP6mcZ2wSQmtq1bMIMiSyP_bVPP9U739PQSp9UE-bITI_6RePBd4FH3n_tfWBhlBAHu-Lb9aaboJWwexIlYws=&original_referer=https://zaajira.com/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fzaajira">
               <i class="fa-brands fa-linkedin-in" />
             </a>
           </li>
