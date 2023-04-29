42b309042d60ff7254f2a4fcff7bf1a4e11d78ad
HIMANxxU <himanshu321go@gmail.com>
2023-04-29 18:00:47 +0530

    InterVirtual comp added

--- src/components/Pages/After Login/Jobs/CreateJob/InterviewQues.js
+++ src/components/Pages/After Login/Jobs/CreateJob/InterviewQues.js
@@ -1,9 +1,14 @@
 import React from "react";
 import MainNavbar from "../../../../CommonComponent/MainNavbar";
 import MainFooter from "../../../../CommonComponent/MainFooter";
+import InterVirtual from "../InterVirtual";
 
 const InterviewQues = () => {
-  return <div></div>;
+  return (
+    <div>
+      <InterVirtual />
+    </div>
+  );
 };
 
 export default InterviewQues;
---
+++ src/components/Pages/After Login/Jobs/InterVirtual.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import "./InterVirtual.css";
+
+export default function InterVirtual() {
+  const [arrVal, setarrVal] = useState([{ value: "" }, { value: "" }]);
+  const addOption = () => {
+    setarrVal([...arrVal, { placeholder: "", value: "" }]);
+  };
+  const removeUsers = (i) => {
+    const newArray = [...arrVal];
+    newArray.splice(i, 1);
+    setarrVal(newArray);
+  };
+
+  return (
+    <div>
+      <div className="mainbox">
+        <div className="firstfield comman">
+          {" "}
+          <div>
+            <input
+              type="text"
+              placeholder="Have you created smart contracts before?"
+            />
+          </div>
+          <div className="marginIcon">
+            {" "}
+            <i
+              class="fa-solid fa-trash-can"
+              style={{ color: "red", fontSize: "25px" }}
+            />
+          </div>
+        </div>
+        {arrVal.map((item, i) => (
+          <div className="comman">
+            {" "}
+            <div>
+              <input type="text" placeholder={"Enter Option " + (i + 1)} />
+            </div>
+            <div style={{ display: "flex" }}>
+              <div>
+                <input
+                  type="checkbox"
+                  style={{
+                    fontSize: "10px",
+                    height: "17px",
+                    border: "2px solid",
+                    width: "30px",
+                    display: "inline",
+                  }}
+                />
+              </div>
+              <div style={{ width: "70px", fontWeight: "500" }}>
+                Mark As Correct
+              </div>
+            </div>
+            <div style={{ marginLeft: "20px" }}>
+              <button onClick={() => removeUsers(i)}>
+                <i
+                  class="fa-solid fa-trash-can"
+                  style={{ color: "red", fontSize: "25px" }}
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {/* <div className="comman">
+          {" "}
+          <div>
+            <input
+              type="text"
+              placeholder="Enter Option 1
+"
+            />
+          </div>
+          <div style={{ display: "flex" }}>
+            <div>
+              <input
+                type="checkbox"
+                style={{
+                  fontSize: "10px",
+                  height: "17px",
+                  border: "2px solid",
+                  width: "30px",
+                  display: "inline",
+                }}
+              />
+            </div>
+            <div style={{ width: "70px", fontWeight: "500" }}>
+              Mark As Correct
+            </div>
+          </div>
+          <div style={{ marginLeft: "20px" }}>
+            <i
+              class="fa-solid fa-trash-can"
+              style={{ color: "red", fontSize: "25px" }}
+            />
+          </div>
+        </div> */}
+        <div>
+          <button style={{ width: "17rem" }} onClick={addOption}>
+            ADD OPTION +
+          </button>
+        </div>
+
+        <div
+          className="comman lastfield"
+          style={{ display: "block", fontSize: "1.2rem" }}
+        >
+          <label
+            htmlFor="Score"
+            style={{ display: "block", fontWeight: "500" }}
+          >
+            Score
+          </label>
+
+          <input type="number" placeholder="0" />
+        </div>
+      </div>
+    </div>
+  );
+}
